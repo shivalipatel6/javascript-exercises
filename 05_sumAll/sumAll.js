19d24e06a399ee1e65280b99171cf7e41ecc00a3
shivalipatel6 <shivalipatel6@gmail.com>
2023-12-19 12:33:10 -0500

    created and passed tests for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+if(num1 < 0 || num2 <0){
+    return "ERROR";
+}// negative number error
+if(typeof num1 != "number" || typeof num2 != "number"){
+    return "ERROR"
+}// non number input error
+let max;
+let min;
+if(num2 > num1){
+max = num2;
+min = num1;
+}
+else{
+    max = num1;
+    min = num2;
+}
 
-};
+let sum = 0;
 
+while(min <= max){
+    sum = sum +min;
+    min++;
+}
+return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
