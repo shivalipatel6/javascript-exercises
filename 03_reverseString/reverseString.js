51c434ce58aea5c3bc3d43baee02f7e7bed1f538
shivalipatel6 <shivalipatel6@gmail.com>
2023-12-19 06:18:42 -0500

    Created and Passed tests for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let finalstring = "";
 
+for(let i = (string.length -1); i > -1; i--){
+    finalstring = finalstring + string.charAt(i);
+
+}
+
+return finalstring;
 };
 
 // Do not edit below this line
