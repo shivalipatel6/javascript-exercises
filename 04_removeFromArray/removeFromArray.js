6bd6c2fe492b3c2ea09c865320c2052a238fd6ae
shivalipatel6 <shivalipatel6@gmail.com>
2023-12-19 12:09:50 -0500

    Completed and passed tests for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary, ...args) {
 
+const newary = [];
+
+ary.forEach((item) => {
+    if(!args.includes(item)){
+        newary.push(item);
+    }
+
+})
+
+return newary;
 };
 
 // Do not edit below this line
